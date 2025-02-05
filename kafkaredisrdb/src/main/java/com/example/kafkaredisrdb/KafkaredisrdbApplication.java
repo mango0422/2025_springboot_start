33b895cbe18b0062b825eb935d61a79817180f61
mango_pc <tom990422@gmail.com>
2025-02-06 02:30:40 +0900

    begin project kafkaredisrdb

---
+++ kafkaredisrdb/src/main/java/com/example/kafkaredisrdb/KafkaredisrdbApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaredisrdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaredisrdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaredisrdbApplication.class, args);
+	}
+
+}
---
+++ kafkaredisrdb/src/test/java/com/example/kafkaredisrdb/KafkaredisrdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaredisrdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaredisrdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
